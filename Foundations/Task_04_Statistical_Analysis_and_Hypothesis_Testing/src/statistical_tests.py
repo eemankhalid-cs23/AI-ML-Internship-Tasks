76cdf5a6a4fd294725626fef7f148cd92548c4a8
eemankhalid-cs23 <eemankhalid995@gmail.com>
2026-08-16 12:49:18 -0700

    Implement statistical test helpers

--- Foundations/Task_04_Statistical_Analysis_and_Hypothesis_Testing/src/statistical_tests.py
+++ Foundations/Task_04_Statistical_Analysis_and_Hypothesis_Testing/src/statistical_tests.py
@@ -1,1 +1,25 @@
+from scipy.stats import ttest_ind, mannwhitneyu, f_oneway
 
+
+def welch_t_test(group1, group2):
+    """
+    Performs Welch's independent samples t-test.
+    Used when two groups have unequal variances.
+    """
+    return ttest_ind(group1, group2, equal_var=False)
+
+
+def mann_whitney_test(group1, group2):
+    """
+    Performs a two-sided Mann-Whitney U test.
+    Used as a non-parametric alternative for comparing two groups.
+    """
+    return mannwhitneyu(group1, group2, alternative="two-sided")
+
+
+def welch_anova(*groups):
+    """
+    Performs Welch's ANOVA for comparing multiple groups
+    when group variances are unequal.
+    """
+    return f_oneway(*groups, equal_var=False)
